f5296455a2b3153199c2461d94e07c02ccae3b40
qwerty <hunter_x5_95@mail.ru>
2026-03-09 22:26:29 +0300

    working on magnetometer calibration script

--- uavconf/plots/magthrust.r
+++ uavconf/plots/magthrust.r
@@ -5,7 +5,8 @@ lpf1t <- function(data, t) {
 	s = 0;
 
 	alpha = exp(-1.0 / freq / t)
-	
+
+	s = mean(data[1:freq]);
 	for (i in 1:length(data)) {
 		s = alpha * s + (1 - alpha) * data[i];
 		r[i] = s;
@@ -14,7 +15,7 @@ lpf1t <- function(data, t) {
 	return(r);
 }
 
-logplot <- function(title, values, yfrom, yto, line) {
+logplot <- function(title, values, yfrom, yto) {
 	plot(values, ylim = c(yfrom, yto), type = "l",
 		xaxs="i", col = "blue", lwd = 5, axes=FALSE);
 	legend("topright", legend = title, col = "blue", cex=15);
@@ -25,6 +26,19 @@ logplot <- function(title, values, yfrom, yto, line) {
 		mgp = c(10, 10, 0), las=1)
 }
 
+magimage <- function(filename, magx, magy, magz, thrust) {
+	png(filename, width = 10000, height = 10000);
+
+	par(mfrow = c(4, 1))
+	par(mar = c(20, 50, 0, 0), cex.axis=10,
+		mgp = c(15, 15, 0), tck=-0.05)
+
+	logplot("x", magx, min(magx), max(magx))
+	logplot("y", magy, min(magy), max(magy))
+	logplot("z", magz, min(magz), max(magz))
+	logplot("thrust", thrust, min(thrust), max(thrust))
+}
+
 args = commandArgs(trailingOnly=TRUE)
 
 t = read.table(args[1], header = FALSE, sep = " ");
@@ -33,6 +47,7 @@ freq = 256
 
 start = 0
 end = 2 * nrow(t) / 3
+samplescount = end - start
 
 magx = t[start:end, 2];
 magy = t[start:end, 3];
@@ -46,29 +61,27 @@ magx_p = lpf1t(magx, 0.025)
 magy_p = lpf1t(magy, 0.025)
 magz_p = lpf1t(magz, 0.025)
 
-startpos = 3 * freq
-
 thrust = 0;
-for (i in 1:length(m1)) {
+for (i in 1:samplescount) {
 	thrust[i] = (m1[i] + m2[i] + m3[i] + m4[i]) / 4.0;
 	if (thrust[i] < 0.0) thrust[i] = 0;
 }
 
-avgx = avgy = avgz = 0.0
-for (i in 1:startpos) {
-	avgx = avgx + magx_p[i];
-	avgy = avgy + magy_p[i];
-	avgz = avgz + magz_p[i];
+startpos = 0
+for (i in 1:samplescount) {
+	startpos = startpos + 1
+	if (thrust[i] > 0.001)
+		break;
 }
 
-avgx = avgx / (startpos);
-avgy = avgy / (startpos);
-avgz = avgz / (startpos);
+avgx = mean(magx_p[1:startpos])
+avgy = mean(magy_p[1:startpos])
+avgz = mean(magz_p[1:startpos])
 
 diffx = diffy = diffz = 0.0
 cnt = 0
-for (i in 1:length(magx_p)) {
-	if (thrust[i] < 0.0001)
+for (i in 1:samplescount) {
+	if (thrust[i] < 0.05)
 		next
 
 	cnt = cnt + 1
@@ -82,32 +95,17 @@ diffx = -diffx / cnt
 diffy = -diffy / cnt
 diffz = -diffz / cnt
 
+printf("starting sample: %d", startpos)
 printf("diff x: %f", diffx);
 printf("diff y: %f", diffy);
 printf("diff z: %f", diffz);
 
-png("magorig.png", width = 10000, height = 10000);
+magimage("magorig.png", magx_p, magy_p, magz_p, thrust)
 
-par(mfrow = c(4, 1))
-par(mar = c(20, 50, 0, 0), cex.axis=10, mgp = c(15, 15, 0), tck=-0.05)
-
-logplot("x", magx_p, min(magx_p), max(magx_p), linepos)
-logplot("y", magy_p, min(magy_p), max(magy_p), linepos)
-logplot("z", magz_p, min(magz_p), max(magz_p), linepos)
-logplot("thrust", thrust, min(thrust), max(thrust), linepos)
-
-for (i in 1:length(magx_p)) {
-	magx_p[i] = magx_p[i] + 1942.314 * (thrust[i]);
-	magy_p[i] = magy_p[i] - 125.647 * (thrust[i]);
-	magz_p[i] = magz_p[i] + 351.655 * (thrust[i]);
+for (i in 1:samplescount) {
+	magx_p[i] = magx_p[i] + diffx * (thrust[i]);
+	magy_p[i] = magy_p[i] + diffy * (thrust[i]);
+	magz_p[i] = magz_p[i] + diffz * (thrust[i]);
 }
 
-png("magcorrected.png", width = 10000, height = 10000);
-
-par(mfrow = c(4, 1))
-par(mar = c(20, 50, 0, 0), cex.axis=10, mgp = c(15, 15, 0), tck=-0.05)
-
-logplot("x", magx_p, min(magx_p), max(magx_p), linepos)
-logplot("y", magy_p, min(magy_p), max(magy_p), linepos)
-logplot("z", magz_p, min(magz_p), max(magz_p), linepos)
-logplot("thrust", thrust, min(thrust), max(thrust), linepos)
+magimage("magcorrected.png", magx_p, magy_p, magz_p, thrust)
